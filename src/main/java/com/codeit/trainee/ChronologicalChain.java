dea19064d1ca4b988e3265dd75e4f26a3896087e
ArtemEkker <ekkerartem@gmail.com>
2018-12-16 18:07:10 +0200

    Main interface and first implementation declared.

---
+++ src/main/java/com/codeit/trainee/ChronologicalChain.java
@@ -0,0 +1,14 @@
+package com.codeit.trainee;
+
+/**
+ * Main class, that is reading JSON and writing it to console as chronological chain of events.
+ * @see <a href="https://drive.google.com/file/d/1FI3vPdp6Wc_FK78fB1Sb6cvtJs2oyc0u/edit">JSON document that is used.</a>
+ */
+public interface ChronologicalChain {
+
+    /**
+     * Main method, that is reading JSON and writing it to console as chronological chain of events.
+     */
+    void start();
+
+}
---
+++ src/main/java/com/codeit/trainee/impl/ChronologicalChainImpl.java
@@ -0,0 +1,9 @@
+package com.codeit.trainee.impl;
+
+import com.codeit.trainee.ChronologicalChain;
+
+public class ChronologicalChainImpl implements ChronologicalChain {
+    public void start() {
+
+    }
+}
