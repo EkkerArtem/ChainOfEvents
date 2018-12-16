37b5b669c0f855d8b715126bae9bd69006cba163
ArtemEkker <ekkerartem@gmail.com>
2018-12-16 19:15:06 +0200

    Main method start() in ChronologicalChainImpl implemented.

--- src/main/java/com/codeit/trainee/ChronologicalChain.java
+++ src/main/java/com/codeit/trainee/ChronologicalChain.java
@@ -1,14 +1,22 @@
 package com.codeit.trainee;
 
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
 /**
  * Main class, that is reading JSON and writing it to console as chronological chain of events.
+ *
  * @see <a href="https://drive.google.com/file/d/1FI3vPdp6Wc_FK78fB1Sb6cvtJs2oyc0u/edit">JSON document that is used.</a>
  */
 public interface ChronologicalChain {
 
     /**
      * Main method, that is reading JSON and writing it to console as chronological chain of events.
+     *
+     * @throws ParseException        that occurs when failed to parse a String that must be in Date format - mm:ss.
+     * @throws InterruptedException  thrown when a thread is waiting, sleeping, or otherwise occupied, and the thread is interrupted, either before or during the activity. It can be thrown in this fragment of code {@code TimeUnit.SECONDS.sleep()}
+     * @throws FileNotFoundException if there is no events.json file.
      */
-    void start();
+    void start() throws ParseException, InterruptedException, FileNotFoundException;
 
 }
--- src/main/java/com/codeit/trainee/impl/ChronologicalChainImpl.java
+++ src/main/java/com/codeit/trainee/impl/ChronologicalChainImpl.java
@@ -1,9 +1,31 @@
 package com.codeit.trainee.impl;
 
 import com.codeit.trainee.ChronologicalChain;
+import com.codeit.trainee.converters.Packer;
+
+import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ChronologicalChainImpl implements ChronologicalChain {
-    public void start() {
 
+    public void start() throws ParseException, InterruptedException, FileNotFoundException {
+        Packer packer = new Packer();
+        Map<Date, String> map = packer.pack();
+        DateFormat dateFormatFromJSON = new SimpleDateFormat("mm:ss");
+        DateFormat dateFormatSeconds = new SimpleDateFormat("ss");
+        DateFormat dateFormatMinutes = new SimpleDateFormat("mm");
+        int startTime = 0;
+
+        for (Map.Entry<Date, String> entry : map.entrySet()) {
+            int eventTimeInSeconds = Integer.parseInt(dateFormatSeconds.format(entry.getKey())) + Integer.parseInt(dateFormatMinutes.format(entry.getKey())) * 60;
+            TimeUnit.SECONDS.sleep(eventTimeInSeconds - startTime);
+            startTime = eventTimeInSeconds;
+            System.out.println(dateFormatFromJSON.format(entry.getKey()) + " " + entry.getValue());
+        }
     }
 }
