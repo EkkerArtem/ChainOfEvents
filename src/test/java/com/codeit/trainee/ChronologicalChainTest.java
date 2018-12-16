94eb5b36ea65e02f728108b8489cfae5820aacbb
ArtemEkker <ekkerartem@gmail.com>
2018-12-16 19:15:47 +0200

    Test class refactored. Now throws ParseException, InterruptedException and FileNotFoundException.

--- src/test/java/com/codeit/trainee/ChronologicalChainTest.java
+++ src/test/java/com/codeit/trainee/ChronologicalChainTest.java
@@ -4,10 +4,13 @@ import com.codeit.trainee.impl.ChronologicalChainImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
 public class ChronologicalChainTest {
 
     @Test
-    public void SpeedTest() {
+    public void SpeedTest() throws ParseException, InterruptedException, FileNotFoundException {
         int supposedRunTime = (1 * 60 + 15) * 1000;
         long start = System.currentTimeMillis();
 
